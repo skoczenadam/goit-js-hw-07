f016da26b9b215f302ff20c7461cf9ccb5d6fe25
Adam Skoczeń <adamsk91@wp.pl>
2024-07-01 17:33:45 +0200

    task 3 complete?

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", event => {
+  output.textContent = event.currentTarget.value.trim();
+    if (event.currentTarget.value === " " || event.currentTarget.value === "") {
+      event.currentTarget.value = "";
+      output.textContent = "Anonymous";
+  }
+});
